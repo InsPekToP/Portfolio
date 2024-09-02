31f35d0bf62ed2133c5fac451d65a21cc73e373e
IsPekToP <inspektop1@gmail.com>
2024-09-02 19:56:44 +0200

    добавил убирание меню на телефонах

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 let menu = document.querySelector('#menu');
 let openMenu = document.querySelector('header');
+let navLinks = document.querySelectorAll('.navbar'); // выбираем все элементы навигации
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     openMenu.classList.toggle('active')
 }
+
+// Добавляем обработчик события для каждого элемента навигации
+navLinks.forEach(link => {
+    link.onclick = () => {
+        menu.classList.remove('fa-times'); // закрываем меню
+        openMenu.classList.remove('active'); // закрываем меню
+    }
+});
