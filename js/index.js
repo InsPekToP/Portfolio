3ebf37a9c2bade2b2369923690fc972fc15edab6
IsPekToP <inspektop1@gmail.com>
2024-09-03 18:52:13 +0200

    увеличение translation.json

--- js/index.js
+++ js/index.js
@@ -14,3 +14,33 @@ navLinks.forEach(link => {
         openMenu.classList.remove('active'); // закрываем меню
     }
 });
+
+let translations = {}; // Объект для хранения переводов
+let currentLanguage = 'en'; // Язык по умолчанию
+
+// Функция для загрузки переводов из JSON-файла
+async function loadTranslations() {
+  try {
+    const response = await fetch('translations.json');
+    translations = await response.json();
+    applyTranslations(); // Применяем переводы после загрузки
+  } catch (error) {
+    console.error('Ошибка загрузки переводов:', error);
+  }
+}
+
+// Функция для применения переводов на странице
+function applyTranslations() {
+  document.getElementById('name').textContent = translations[currentLanguage].name;
+//   document.getElementById('about-text').textContent = translations[currentLanguage].about;
+//   document.getElementById('contact-text').textContent = translations[currentLanguage].contact;
+}
+
+// Функция для смены языка
+function changeLanguage(lang) {
+  currentLanguage = lang;
+  applyTranslations();
+}
+
+// Загрузка переводов при загрузке страницы
+window.onload = loadTranslations;
