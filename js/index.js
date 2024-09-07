c8a9b395d5a93bdfa45b20da7f6025e11ad1bc5d
IsPekToP <inspektop1@gmail.com>
2024-09-07 13:28:40 +0200

    добавил обновление языка резюме при разных кликах

--- js/index.js
+++ js/index.js
@@ -29,6 +29,46 @@ async function loadTranslations() {
   }
 }
 
+// Массив с ссылками на резюме для разных языков
+const resumes = {
+  en: {
+      href: "resume/CV_Holykov_developer_en.docx",
+      download: "CV_Holykov-developer_en.docx"
+  },
+  ru: {
+      href: "resume/CV_Holykov_developer_ru.docx",
+      download: "CV_Holykov-developer_ru.docx"
+  },
+  pl: {
+      href: "resume/CV_Holykov_developer_pl.docx",
+      download: "CV_Holykov-developer_pl.docx"
+  }
+};
+
+// Функция смены языка, обновления текста и ссылки на резюме
+function changeLanguage(lang) {
+  currentLanguage = lang;  // обновляем текущий язык
+
+  // Обновляем текст на странице в зависимости от выбранного языка
+  applyTranslations();
+
+  // Обновляем ссылку на резюме в зависимости от выбранного языка
+  updateResumeLink();
+}
+
+// Обновляем ссылку на резюме
+function updateResumeLink() {
+  const resumeLink = document.getElementById('resume_link');
+  const resumeData = resumes[currentLanguage];  // получаем данные о резюме для текущего языка
+
+  // Обновляем href и download атрибуты
+  if (resumeData) {
+      resumeLink.href = resumeData.href;
+      resumeLink.download = resumeData.download;
+  }
+}
+
+
 // Функция для применения переводов на странице
 function applyTranslations() {
   document.getElementById('name').textContent = translations[currentLanguage].name;
@@ -75,6 +115,9 @@ function applyTranslations() {
   document.getElementById('name_placeholder').setAttribute('placeholder', translations[currentLanguage].name_placeholder);
   document.getElementById('message_placeholder').setAttribute('placeholder', translations[currentLanguage].message_placeholder);
   document.getElementById('send').textContent = translations[currentLanguage].send;
+
+  // Обновляем резюме ссылку
+  updateResumeLink();
 }
 
 // Функция для смены языка
@@ -85,11 +128,3 @@ function changeLanguage(lang) {
 
 // Загрузка переводов при загрузке страницы
 window.onload = loadTranslations;
-
-// // Функция для обновления текста
-// function updateAboutMeText() {
-//   document.getElementById('about_me').innerHTML = translations[currentLanguage].about_me;
-// }
-
-// // Вызов функции для обновления текста
-// updateAboutMeText();
